589d8708d0fc2a4cac5f4e95d855fbbb04596b51
mayumiyamada <y.mayumi1011@gmail.com>
2021-01-11 12:09:12 +0900

    シンボル型に変更

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -42,7 +42,7 @@ class CalendarsController < ApplicationController
       end
 
 
-      days = {month:(@todays_date + x).month, date:@todays_date.day + x, plans:today_plans, :wday => wdays[wday_num] }
+      days = {month:(@todays_date + x).month, date:@todays_date.day + x, plans:today_plans, wday:wdays[wday_num] }
 
       @week_days.push(days)
     end
